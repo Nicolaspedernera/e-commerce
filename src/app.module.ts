38443646f746235d7615731f998bb4c803fbb9f4
Nicolas Pedernera <nicopedernera12@gmail.com>
2022-10-31 22:33:10 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     autoLoadEntities:true,
-    synchronize:true, 
-    
-
-
-
+    synchronize:true,
   })
 ],
   
