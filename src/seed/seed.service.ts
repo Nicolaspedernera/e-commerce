b1311edacef84585bec76731423a21e228087eb0
Nicolas Pedernera <nicopedernera12@gmail.com>
2022-11-07 15:47:25 -0300

    Add seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSeedDto } from './dto/create-seed.dto';
-import { UpdateSeedDto } from './dto/update-seed.dto';
+import { ProductsService } from './../products/products.service';
 
 @Injectable()
 export class SeedService {
-  create(createSeedDto: CreateSeedDto) {
-    return 'This action adds a new seed';
+  constructor(private readonly productsService: ProductsService) {}
+  async runSeed() {
+    return await this.reBuildProducs();
   }
 
-  findAll() {
-    return `This action returns all seed`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} seed`;
-  }
-
-  update(id: number, updateSeedDto: UpdateSeedDto) {
-    return `This action updates a #${id} seed`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seed`;
+  private async reBuildProducs() {
+    await this.productsService.deleteAllProductsSeed();
+    return { message: 'All products deleted' };
   }
 }
